bf52295f99f00f9ca534b363e9c8533febb58f57
lilo <lilo.marino@hotmail.com>
2023-04-22 17:41:05 -0300

    chore: Organização básica

---
+++ arvore.h
@@ -0,0 +1,11 @@
+#ifndef ARVORE_H
+#define ARVORE_H
+
+#define CAPACIDADE_DA_MOCHILA 400
+#define NUMERO_DE_ITEMS 4
+
+
+
+
+
+#endif
---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "arvore.h"
+
+struct StElemento 
+{
+    int Prioridade;
+    int Tamanho;
+};
+
+typedef struct StElemento Elemento;
+
+int main()
+{
+    Elemento Itens[NUMERO_DE_ITEMS];
+
+}
