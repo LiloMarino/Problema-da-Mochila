7fe785c9d00a8ea3b6c43a7b621ccb38431fba2d
LiloMarino <lilo.marino@hotmail.com>
2023-04-25 17:09:18 -0300

    chore: save

--- main.c
+++ main.c
@@ -6,14 +6,26 @@ struct StElemento
 {
     int Prioridade;
     int Tamanho;
-    int MaxCapac;
+    int MaxQntde;
 };
 
 typedef struct StElemento Elemento;
 
+void ImprimeVetorInt(int vetor_int[], int ni)
+{
+    // Imprime o vetor int
+    for (int i = 0; i < ni; i++)
+    {
+        printf("%d ", vetor_int[i]);
+    }
+    printf("\n");
+}
+
 int main()
 {
     Elemento Itens[NUMERO_DE_ITEMS];
+    int Quantidades[NUMERO_DE_ITEMS];
+    int ni = sizeof(Quantidades) / sizeof(int);
     ArqCmds Dados = abreArquivoCmd("dados.txt");
     char *str = NULL;
     int i;
@@ -26,14 +38,35 @@ int main()
     /*Máx de cada item*/
     for (i = 0; i < NUMERO_DE_ITEMS; i++)
     {
-        Itens[i].MaxCapac = CAPACIDADE_DA_MOCHILA / Itens[i].Tamanho;
+        Itens[i].MaxQntde = CAPACIDADE_DA_MOCHILA / Itens[i].Tamanho;
+        Quantidades[i] = 0;
     }
-    /*Verificações*/
+    /*Verificações baseados na capacidade*/
     for (int j = 0; j < NUMERO_DE_ITEMS; j++)
     {
-        for (i = Itens[j].MaxCapac; i >= 0; i--)
+        for (i = Itens[j].MaxQntde; i >= 0; i--)
         {
-            printf("%d\n", i);
+            int CapacRestante = CAPACIDADE_DA_MOCHILA;
+            /* Variações a partir do item i */
+            if (CapacRestante - Itens[j].Tamanho * i >= 0)
+            {
+                CapacRestante -= Itens[j].Tamanho * i;
+                Quantidades[j] = i;
+            }
+            /*Verificar os próximos*/
+            for (int k = j + 1; k < NUMERO_DE_ITEMS; k++)
+            {
+                for (int qntde = Itens[k].MaxQntde; qntde >= 0; qntde--)
+                {
+                    if (CapacRestante - Itens[k].Tamanho * qntde >= 0)
+                    {
+                        CapacRestante -= Itens[k].Tamanho * qntde;
+                        Quantidades[k] = qntde;
+                        break;
+                    }
+                }
+            }
+            ImprimeVetorInt(Quantidades, ni);
         }
     }
 }
