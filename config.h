19ffd7cf7b141d8880d3fecd55119f7c5ac9af0e
LiloMarino <lilo.marino@hotmail.com>
2023-05-02 17:03:05 -0300

    chore: Feito algumas anotações

--- config.h
+++ config.h
@@ -3,12 +3,15 @@
 
 /*Configurações*/
 #define CAPACIDADE_DA_MOCHILA 400
-#define NUMERO_DE_ITEMS 4
+#define NUMERO_DE_ITEMS 6
 #define GERAR 0 /* 1 = True, 0 = False*/
 #define SHOW_ONLY_ANSWER 0 /* 1 = True, 0 = False, Ignora SHOW_ON_TERMINAL*/
 #define SHOW_ON_TERMINAL 1 /* 1 = True, 0 = False*/
-#define METHOD_USE 2 /* 0 = All, 1 = Gulosa, 2 = BranchBound*/
+#define METHOD_USE 0 /* 0 = All, 1 = Gulosa, 2 = BranchBound*/
 
-///@note Mudar a implementação recursiva em iterativa ou, se isso não for possível, otimizar sua função recursiva para reduzir o número de chamadas recursivas necessárias.
-
+/**
+ * @note Mudar a implementação recursiva em iterativa ou, se isso não for possível, 
+ * otimizar sua função recursiva para reduzir o número de chamadas recursivas necessárias.
+ * @note Refazer a função de geração para que esteja ligada ao NUMERO_DE_ITEMS e 
+ * reduza a chance de ter uma prioridade altíssima com um tamanho pequeno por meio da geração individual dos números
 #endif
